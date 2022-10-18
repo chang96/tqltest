9eea1859163f41d96858664734a9e5ceeee5b4db
rahman Orewole <rorewole@gmail.com>
2022-10-18 16:59:37 +0100

    how old route implemented

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, {Express, Request, Response} from "express";
+import {router} from "./routes/index"
 
 const PORT:string = process.env.PORT || "3005";
 
 const app: Express = express();
-
+app.use("/", router)
 app.use("*", function(req: Request, res:Response): void{
     res.status(404).json({"message": "route not found"})
 })
