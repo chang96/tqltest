3699d16b7a74886445ea74f639383c42ac447bc7
rahman Orewole <rorewole@gmail.com>
2022-10-18 17:03:14 +0100

    rate limit with express rate limit done

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, {Express, Request, Response} from "express";
 import {router} from "./routes/index"
+import { rateLimiter } from "./middlewares";
 
 const PORT:string = process.env.PORT || "3005";
 
 const app: Express = express();
+app.use(rateLimiter)
 app.use("/", router)
 app.use("*", function(req: Request, res:Response): void{
     res.status(404).json({"message": "route not found"})
