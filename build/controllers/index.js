6cc24adeb874ace0b144c766745394901aff3624
rahman Orewole <rorewole@gmail.com>
2022-10-19 16:37:02 +0100

    edge cases for age handled

--- build/controllers/index.js
+++ build/controllers/index.js
@@ -9,6 +9,6 @@ const calculateAgeController = function (req, res) {
     if (age)
         res.status(200).json({ "statusCode": 200, "age": age });
     else
-        res.status(400).json({ "statusCode": 400, "age": `${age}`, "error": "invalid age parameter" });
+        res.status(400).json({ "statusCode": 400, "age": `${age}`, "response": {"status": 400, "data": {"error": "invalid parameter"}} });
 };
 exports.calculateAgeController = calculateAgeController;
